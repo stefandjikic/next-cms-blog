921218d4c95e8d32c199f04ef58bfeb1390a407b
Stefan Djikic <stefandjikic@outlook.com>
2023-12-03 22:47:36 +0100

    Code Cleanup

--- app/(client)/layout.tsx
+++ app/(client)/layout.tsx
@@ -1,10 +1,9 @@
 import type { Metadata } from "next";
-import { Inter, Fira_Code } from "next/font/google";
+import { Fira_Code } from "next/font/google";
 import "./globals.css";
 import Navbar from "../components/Navbar";
 import { Provider } from "../utils/Provider";
 
-// const inter = Inter({ subsets: ["latin"] });
 const firaCode = Fira_Code({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
